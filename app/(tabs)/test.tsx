c4e05c52e005bd6bce9c5e93d5cf6790f1f48f66
sjbworks <sacred.jizo.bosatz@gmail.com>
2026-04-16 23:37:07 +0900

    Add test page

---
+++ app/(tabs)/test.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function TestScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Test</Text>
+    </View>
+  );
+}
