0b039b023e81ebfbe9edfe612af5c37a2a8c6afb
sjbworks <sacred.jizo.bosatz@gmail.com>
2026-04-17 22:29:48 +0900

    Add FlatList

--- app/(tabs)/test.tsx
+++ app/(tabs)/test.tsx
@@ -1,9 +1,32 @@
-import { Text, View } from "react-native";
+import { FlatList, Image, Text, View } from "react-native";
+const DATA = [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+    image: require("@/assets/images/react-logo.png"),
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
+    image: require("@/assets/images/react-logo.png"),
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+    image: require("@/assets/images/react-logo.png"),
+  },
+];
 
 export default function TestScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Test</Text>
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => (
+          <Image source={item.image} style={{ width: 100, height: 100 }} />
+        )}
+      />
     </View>
   );
 }
